211139dd9f3d7b3856e40837b84c959cdb86a332
Mikhail Poluboyarinov <mikhailpoluboyarinov@yandex.ru>
2022-09-06 18:34:01 +0300

    поправил env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorizedError401');
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
